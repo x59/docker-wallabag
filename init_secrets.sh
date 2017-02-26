38450500cc1101e33c98df91c54e97ff4e42de59
Yehuda Deutsch <yeh@uda.co.il>
2017-02-26 22:04:23 +0200

    Ready to use docker-compose

---
+++ init_secrets.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+random-string()
+{
+    cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1
+}
+
+set +x
+
+DB_PASSWORD=`random-string`
+ENV_SECRET=`random-string`
+
+cp env.sh `date +"%Y%m%d_%H%M%S"`_env.sh
+
+sed -e "s/MYSQL_ROOT_PASSWORD=.*/MYSQL_ROOT_PASSWORD=${DB_PASSWORD}/" \
+    -e "s/POSTGRES_PASSWORD=.*/POSTGRES_PASSWORD=${DB_PASSWORD}/" \
+    -e "s/SYMFONY__ENV__DATABASE_PASSWORD=.*/SYMFONY__ENV__DATABASE_PASSWORD=${DB_PASSWORD}/" \
+    -e "s/SYMFONY__ENV__SECRET=.*/SYMFONY__ENV__SECRET=${ENV_SECRET}/" \
+    -i env.sh
