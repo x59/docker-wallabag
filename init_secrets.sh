394ad999622b5bec1bbc6d64092384ba568cad76
Yehuda Deutsch <yeh@uda.co.il>
2017-02-26 22:53:58 +0200

    Separate root and non root pass

--- init_secrets.sh
+++ init_secrets.sh
@@ -7,13 +7,14 @@ random-string()
 
 set +x
 
+DB_ROOT_PASSWORD=`random-string`
 DB_PASSWORD=`random-string`
 ENV_SECRET=`random-string`
 
 cp env.sh `date +"%Y%m%d_%H%M%S"`_env.sh
 
-sed -e "s/MYSQL_ROOT_PASSWORD=.*/MYSQL_ROOT_PASSWORD=${DB_PASSWORD}/" \
-    -e "s/POSTGRES_PASSWORD=.*/POSTGRES_PASSWORD=${DB_PASSWORD}/" \
+sed -e "s/MYSQL_ROOT_PASSWORD=.*/MYSQL_ROOT_PASSWORD=${DB_ROOT_PASSWORD}/" \
+    -e "s/POSTGRES_PASSWORD=.*/POSTGRES_PASSWORD=${DB_ROOT_PASSWORD}/" \
     -e "s/SYMFONY__ENV__DATABASE_PASSWORD=.*/SYMFONY__ENV__DATABASE_PASSWORD=${DB_PASSWORD}/" \
     -e "s/SYMFONY__ENV__SECRET=.*/SYMFONY__ENV__SECRET=${ENV_SECRET}/" \
     -i env.sh
